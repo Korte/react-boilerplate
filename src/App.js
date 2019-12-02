b94784d86c9df80ff297ad597076d1d7dc0a181d
Sonny Korte <sonny@korte.dk>
2019-12-02 13:49:49 +0100

    update code to be in line with the tutorial

--- src/App.js
+++ src/App.js
@@ -1,21 +1,29 @@
 import React from 'react'
 
 class App extends React.Component {
-    state = {
-        count: 0
-    }
-    
-    render(){
+  state = {
+    count: 0
+  };
+  
+  increment = () => {
+    this.setState(state => ({ count: state.count + 1 }));
+  };
 
-        return (
-            <div>
-                <h1>Hello world!!!</h1>
-                <h2>Count: {this.state.count} </h2>
-                <button onClick={() => this.setState(state => ({count: state.count + 1}))}>+</button>
-                <button onClick={() => this.setState(state => ({count: state.count - 1}))}>-</button>
-            </div>
-        )
-    }
+  decrement = () => {
+    this.setState(state => ({ count: state.count - 1 }));
+  };
+
+  render() {
+    const { count } = this.state;
+    return (
+      <div>
+        <h1>Hello world!!!</h1>
+        <h2>Count: {this.state.count} </h2>
+        <button onClick={this.increment}>+</button>
+        <button onClick={this.decrement}>-</button>
+      </div>
+    );
+  }
 }
 
 export default App
